213f1169be2958552c143e0120f922f3f4fb4968
nshmdayo <asrgmt24@gmail.com>
2025-10-19 21:25:42 +0900

    refactoring main.go

--- main.go
+++ main.go
@@ -211,46 +211,14 @@ func generateIndexPage(config Config, posts []Post) {
 
 	err = tmpl.Execute(f, data)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error executing template: %v", err)
 	}
 }
 
 func generateBlogPages(config Config, posts []Post) {
-	tmpl := `<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{.Post.Title}} - {{.Config.Name}}</title>
-    <meta name="description" content="{{.Post.Description}}">
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    <main class="container">
-        <article>
-            <h1>{{.Post.Title}}</h1>
-            <div class="date">{{.Post.Date}}</div>
-            {{if .Post.Tags}}
-            <div class="tags" style="margin: 1rem 0;">
-                {{range .Post.Tags}}
-                <span class="tag">{{.}}</span>
-                {{end}}
-            </div>
-            {{end}}
-            <div>
-                {{.Post.Content}}
-            </div>
-            <div style="margin-top: 3rem;">
-                <a href="../index.html#blog" class="btn">← Back to Blog</a>
-            </div>
-        </article>
-    </main>
-</body>
-</html>`
-
-	t, err := template.New("post").Parse(tmpl)
+	tmpl, err := template.ParseFiles("template/post.html.tpl")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error parsing post.html.tpl: %v", err)
 	}
 
 	for _, post := range posts {
@@ -265,7 +233,7 @@ func generateBlogPages(config Config, posts []Post) {
 			Post:   &post,
 		}
 
-		err = t.Execute(f, data)
+		err = tmpl.Execute(f, data)
 		if err != nil {
 			log.Printf("Error executing template for post %s: %v", post.Slug, err)
 		}
